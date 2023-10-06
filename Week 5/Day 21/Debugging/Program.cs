ffac7458aacfc769f0ea5e6ebe906d7357e52725
Reinato Teguh Santoso <reinoteguhsts15@gmail.com>
2023-10-06 10:07:49 +0700

    Upload Week 5 - Day 21 project

---
+++ Week 5/Day 21/Debugging/Program.cs
@@ -0,0 +1,19 @@
+﻿static class Program
+{
+	static void Main()
+	{
+		double num1 = 4.0;
+		double num2 = 2.5;
+		double result = Add(num1, num2);
+		
+		Console.WriteLine($"{num1} + {num2} = {result}");
+		
+		var x = Console.ReadLine();
+		Console.WriteLine(x);
+	}
+	
+	static double Add(double a, double b)
+	{
+		return a * b; 
+	}
+}
---
+++ Week 5/Day 21/PreprocessingDirectives/Program.cs
@@ -0,0 +1,25 @@
+﻿// Defaultly, C# will using DEBUG as preprocessing directives
+// To run in other mode of P.D.:
+// dotnet run -c YOUR_MODE
+
+#define RELEASE
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Start");
+		
+		#if DEBUG
+		Console.WriteLine("Debug mode is activated...");
+		#elif PRODUCTION
+		Console.WriteLine("Production mode is activated...");
+		#elif TEST
+		#warning This is a test mode
+		Console.WriteLine("Test mode is activated...");
+		#else
+		Console.WriteLine("Release mode is activated...");
+		#endif
+		
+		Console.WriteLine("End");
+	}
+}
